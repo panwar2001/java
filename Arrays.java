3417f35d18079873837dcacd98fa2b0dde83ec53
panwar2001 <ayushpanwar691@gmail.com>
2023-08-09 20:08:22 +0530

    add Arrays

---
+++ Arrays.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+public class Array{
+    public static void main(String args[]){
+        Integer nums[]={1,2,3,4,5,6,7,9};
+        int index=Arrays.binarySearch(nums,9);
+        System.out.println(index);        
+        Arrays.sort(nums,(a,b)->b-a);
+        for(int i:nums){
+                System.out.print(i+" ");
+        } 
+        Arrays.fill(nums,0);
+        for(int i:nums){
+                System.out.print(i+" ");
+        }
+    }
+}
